8a4411ea4a8188a4e7eefff20b0fb9b06e979bd9
chenshengjie <15857086605@163.com>
2022-06-19 13:52:58 +0800

    修改gradle版本

--- commonutils/src/main/java/com/sunyard/commonutils/MyToats.java
+++ commonutils/src/main/java/com/sunyard/commonutils/MyToats.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
  * @time 2020/9/18 2:56 PM
  */
 public class MyToats {
-
+//自定义toast
     public void  myToast(Context context,String content){
 
         Toast.makeText(context,content,Toast.LENGTH_SHORT).show();
